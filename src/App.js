5d782ce642d685b27f7870faadcf00c82925be6d
Uttam Pal <stem.uttam@gmail.com>
2025-02-21 17:13:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeTable from "./EmployeeTable";
+
+function App() {
+    return (
+        <div className="App">
+            <EmployeeTable />
+        </div>
+    );
+}
+
+export default App;
